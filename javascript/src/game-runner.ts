7d6546686e3c74898fa1ffe4591d6810d1ef9d35
Linesh <linesh.simpcity@gmail.com>
2019-12-05 23:55:30 +0800

    step-6: renaming

--- javascript/src/game-runner.ts
+++ javascript/src/game-runner.ts
@@ -1,11 +1,11 @@
 import { Game } from "./game";
 
-export function gameRunner(randomInt) {
+export function gameRunner(simulator) {
   const game = new Game();
 
   game.add("Chet");
   game.add("Pat");
   game.add("Sue");
 
-  game.start(randomInt);
+  game.start(simulator);
 }
