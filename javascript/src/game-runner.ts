c0738d0383043f86442a0b3d7fbfe5ca884bd8aa
Linesh <linesh.simpcity@gmail.com>
2019-12-05 23:49:21 +0800

    step-6: move statements for an upcoming method extraction

--- javascript/src/game-runner.ts
+++ javascript/src/game-runner.ts
@@ -2,15 +2,13 @@ import { Game } from "./game";
 
 export function gameRunner(randomInt) {
   // a simulator of a game
-
-  let notAWinner = false;
-
   const game = new Game();
 
   game.add("Chet");
   game.add("Pat");
   game.add("Sue");
 
+  let notAWinner = false;
   do {
     const shouldAnswerQuestion = game.roll(randomInt(6));
     const isGivingCorrectAnswer = randomInt(10) == 7;
